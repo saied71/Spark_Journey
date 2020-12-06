b50341a76d4622f203a3d627a179d731161585e8
Saied <saied.alimoradi@gmail.com>
2020-12-06 22:09:54 +0330

    finishing Day1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Spark_Journey
+
+This repository contains my journey through learning apache spark. For now, I go throw the book "Spark The Definitive Guide" all codes in this part are available under package `saied.spark`, which is arranged day by day as I go through.
+Later on, I will plan to do some real-world projects involving some libraries such as MLlib, GrapghX...
+
+**Day1:** Introduction to spark. overview of different API's and end-to-end example
--- Spark-The-Definitive-Guide_learning/src/main/scala/saied/spark/Day1.scala
+++ Spark-The-Definitive-Guide_learning/src/main/scala/saied/spark/Day1.scala
@@ -1,6 +1,8 @@
 package saied.spark
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j._
+import org.apache.spark.sql.functions.max
+import org.apache.spark.sql.functions.desc
 object Day1 {
   def main(args:Array[String]): Unit ={
 
@@ -19,9 +21,54 @@ object Day1 {
       .csv(data_dir)
 
 
-//    filghtData2015.show(5)
+    filghtData2015.show(5)
     filghtData2015.sort("count").explain()
 
+    spark.conf.set("spark.sql.shuffle.partitions", 5)
+    filghtData2015.createOrReplaceTempView("flight_data_2015")
+
+    val sqlWay = spark.sql("""
+       SELECT DEST_COUNTRY_NAME, count(1)
+       FROM flight_data_2015
+       GROUP BY DEST_COUNTRY_NAME
+       """)
+
+    val dataFrameWay = filghtData2015
+      .groupBy("DEST_COUNTRY_NAME")
+      .count()
+
+    sqlWay.explain()
+    dataFrameWay.explain()
+
+    spark.sql("SELECT max(count) from flight_data_2015").take(1)
+
+    filghtData2015.select(max("count")).take(1)
+
+    val maxSql = spark.sql(
+      """
+        SELECT DEST_COUNTRY_NAME, sum(count) as destination_total
+        FROM flight_data_2015
+        GROUP BY DEST_COUNTRY_NAME
+        ORDER BY sum(count) DESC
+        LIMIT 5
+        """)
+    maxSql.show()
+
+    filghtData2015
+      .groupBy("DEST_COUNTRY_NAME")
+      .sum("count")
+      .withColumnRenamed("sum(count)", "destination_total")
+      .sort(desc("destination_total"))
+      .limit(5)
+      .show()
+
+    filghtData2015
+      .groupBy("DEST_COUNTRY_NAME")
+      .sum("count")
+      .withColumnRenamed("sum(count)", "destination_total")
+      .sort(desc("destination_total"))
+      .limit(5)
+      .explain()
 
 
   }
